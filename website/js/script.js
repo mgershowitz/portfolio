c1f426cf2c97daecdd5a48b0535ddef5a4651b1d
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-06-16 08:37:49 -0400

    profile page starter

--- website/js/script.js
+++ website/js/script.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
+console.log("connected!")
 
 
-  console.log("connected!")
+
+$('#me').hover(function(e) {
+  $(this).attr('src', "./assets/meLinkedin.jpg");
+},
+function(e) {
+  $(this).attr('src', "./assets/mattCongestedCat.png");
+})
+
+$('h1').hover(function(e) {
+  $(this).text('Web Developer!!');
+},
+function(e) {
+  $(this).text('Matt Gershowitz');
+})
+
+$('.personal-links').hover(function(e) {
+  $(this).css('color', 'blue');
+},
+function(e) {
+  $(this).css('color', 'white');
+});
 });

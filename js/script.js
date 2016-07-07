fdfc0e70a16bffb65c3df7f83417e74c74e110e5
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-07-06 23:41:02 -0400

    better now

--- js/script.js
+++ js/script.js
@@ -3,41 +3,59 @@ const greetings = [
                     'Good to see you',
                     'Valar Morghulis',
                     'Don\'t spend all day inside',
-                    'What\'s the deal with airline food?',
+                    'What IS the deal with airline food?',
                     'I believe',
                     'Wubba Lubba Dub Dubs!!!',
                     'Hello',
-                    'Be excellent to each other'
+                    'Be excellent to each other',
+                    "I can show you the world",
+                    'Who wants to be $$$\'aire!',
+                    'Mirror, Mirror',
+                    'Who are you?',
+                    'We goina Sizzler',
+                    'I am the one who knocks!'
                     ]
 
-let welcomeMessage = () => {
-  let i = Math.floor(Math.random()*greetings.length)
-  greetings[i]
-}
 
 
+let i = 0;
 
 
-$(()=> {
 console.log('connected')
+$(()=> {
 
 
-// setInterval(function(){
-//     $("#welcome h1").text(welcomeMessage).fadeIn("slow");
-//     $("#welcome h1").delay(40).fadeOut("slow");
-// }, 18);
 
-// $('#email img').on('mouseover', function(e) {
-//   $(this).toggle('show'/'hide')();
-//   $('#email').text('mattgershowitz1@gmail.com').addClass('animated bounceInUp')
-//   .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-//   $(this).removeClass().addClass('reachOut')
-// });
-// })
+setInterval(function
+  (){
+    $("#welcome h1").addClass('animated bounceOutRight')
+    .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    $(this).removeClass().addClass('title').text(greetings[i]).addClass('animated bounceInLeft')
+    .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    $(this).removeClass().addClass('title');
+  })
+  })
+    i = Math.floor(Math.random()*greetings.length)
+}, 3500);
+
 
 $('#email').on('click', function(e) {
   $(this).text("")
   $('#email img').toggle('show'/'hide')
+})
+
+$('#me').on('mouseover', function(e){
+  $(this).text("FULL STACK!! pew pew pew!!!")
+})
+
+$('#me').on('mouseleave', function(e){
+  $(this).text("Matt Gershowitz")
+})
+
+$('#me').on('click', (e) =>{
+    $('html, body').animate({
+        scrollTop: $("#welcome").offset().top
+    }, 500);
 })
 
 $('#bioB').on('click', (e) =>{
@@ -55,6 +73,12 @@ $('#linksB').on('click', (e) =>{
 $('#projB').on('click', (e) =>{
   $('html, body').animate({
         scrollTop: $("#projects").offset().top -100
+    }, 500);
+})
+
+$('#ringB').on('click', (e) =>{
+  $('html, body').animate({
+        scrollTop: $("#contact").offset().top -100
     }, 500);
 })
 
